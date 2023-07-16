31ee84c81753bff51fb077a45a5d76b8f6dbfb5d
fchpriani <federico.cipriani@y.uno>
2023-07-16 20:55:40 -0300

    meta: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<h1>sveltekit-gh-pages</h1>
+<p>Deployed to GitHub Pages</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<a href="{base}/about">About</a>
+
+<style>
+  @font-face{
+    font-family:parametric glitch;
+    font-style:normal;
+    font-weight:400;
+    src:local('Parametric Glitch'),url('https://fonts.cdnfonts.com/s/55098/Parametric Glitch.woff') format('woff')
+  }
+
+  h1 {
+    font-family: "Parametric Glitch"
+  }
+</style>
